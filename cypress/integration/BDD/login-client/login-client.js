ecdbdf634e5157e22d88dc73e1fbe0502f65e910
unknown <bjoshi@codal.com>
2022-04-27 16:17:15 +0530

    Added parameters in gitignore

--- cypress/integration/BDD/login-client/login-client.js
+++ cypress/integration/BDD/login-client/login-client.js
@@ -1,9 +1,7 @@
 /// <reference types="Cypress" />
-import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+import { Given, When, Then, And } from 'cypress-cucumber-preprocessor/steps'
 import login_PO from '../../../support/pageObjects/login_PO'
 const login = new login_PO();
-
-
 
 Given('User is at the login page', () => {
 
@@ -13,13 +11,14 @@ Given('User is at the login page', () => {
 When('User enters username as {string} and password as {string}', (username, password) => {
     cy.get('#txtUsername').type(username)
     cy.get('#txtPassword').type(password)
+
 })
 
 And('User clicks on login button', () => {
     cy.get('#btnLogin').click()
-    
+
 })
 
 Then('User is able to successfully login to the Website', () => {
-    cy.get('#welcome').should('be.visible', {timeout: 10000})
+    cy.get('#welcome').should('be.visible', { timeout: 10000 })
 })
