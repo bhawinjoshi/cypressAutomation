6ee9473b444a217b0a77a808edb8d043f8b142b6
unknown <bjoshi@codal.com>
2022-05-24 16:32:33 +0530

    updated device token for advisor

--- cypress/integration/Login/login-advisor/login-advisor.js
+++ cypress/integration/Login/login-advisor/login-advisor.js
@@ -1,5 +1,5 @@
 import { Given } from 'cypress-cucumber-preprocessor/steps'
-const userDetails_key_advisor = { "device_token": "3850d0dda1313b43e911da872c2fbfb393beec25", "is_first_login": false }
+const userDetails_key_advisor = { "device_token": "d7e3305f890a99caebe12c8cbb5b1ecabf21581d", "is_first_login": false }
 const user_key_advisor = false;
 const email_key_advisor = "advisorautomationtest@codaldemo.com";
 
