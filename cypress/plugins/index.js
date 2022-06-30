14b035d9f71a8193b40fb060afafe8ce3a553121
parag-codal <pjathar@codal.com>
2022-06-30 16:50:22 +0530

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -21,7 +21,7 @@ module.exports = (on, config) => {
   // To run tests in Chrome incognito mode.
   on("before:browser:launch", (browser, launchOptions) => {
     if (browser.name === "chrome" && browser.isHeadless) {
-      launchOptions.args.push("--disable-gpu");
+      launchOptions.args.push("--disable-gpu --disable-software-rasterizer");
     }
     return launchOptions;
   });
