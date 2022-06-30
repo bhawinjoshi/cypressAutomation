d4c8555b6c93518700d3e0ed65f3255d549437e8
parag-codal <pjathar@codal.com>
2022-06-30 16:56:58 +0530

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -20,8 +20,8 @@ module.exports = (on, config) => {
   on('file:preprocessor', cucumber()) // for Cucumber preProcessor
   // To run tests in Chrome incognito mode.
   on("before:browser:launch", (browser, launchOptions) => {
-    if (browser.name === "chrome" && browser.isHeadless) {
-      launchOptions.args.push("--disable-gpu --disable-software-rasterizer");
+    if (browser.name === 'chrome' && browser.isHeadless) {
+      launchOptions.args.push('--disable-dev-shm-usage');
     }
     return launchOptions;
   });
