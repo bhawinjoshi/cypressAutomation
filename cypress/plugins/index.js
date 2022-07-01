94e7f7b5328cb03446054a0b65cf569a88b8cfb7
parag-codal <pjathar@codal.com>
2022-07-01 14:05:27 +0530

    Update index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -21,6 +21,7 @@ module.exports = (on, config) => {
   // To run tests in Chrome incognito mode.
   on("before:browser:launch", (browser, launchOptions) => {
     if (browser.name === "chrome") {
+      launchOptions.args.push("--no-sandbox");
       launchOptions.args.push("--disable-gpu");
       launchOptions.args.push("--disable-dev-shm-usage");
     }
